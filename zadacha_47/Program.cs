72ee94a6b8249e1112ed088b3a8795437355b7fd
Andrey <6198760@gmail.com>
2022-12-19 00:36:35 +0300

    solve zadacha 47

---
+++ zadacha_47/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+Console.Clear();
+
+void ShowArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] CreateRandomArray(int rows, int columns, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    double[,] array = new double[rows, columns];
+        for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = rand.NextDouble()*10;
+        }
+    }
+    return array;
+}
+
+int EnterNumber(string message)                   //мы передаем текст...какая то переменная какого то типа, которая может принять строку
+{
+    System.Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+//1. получить от пользователя м и н
+int m = EnterNumber("введите значение m: ");
+int n = EnterNumber("введите значение n: ");
+
+//2. создаем двумерный массив и заполняем
+
+double[,] matrix = CreateRandomArray(m, n, 1, 10);                //так создается двумерный массив обратить внимание на [,]
+
+//3. вывести массив
+ShowArray(matrix);
